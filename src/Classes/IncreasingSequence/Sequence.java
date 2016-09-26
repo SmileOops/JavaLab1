e3ffc050bd1c7ac188cdf48cbecf49352bea48c5
Vlad <diablo3mail111@gmail.com>
2016-09-27 00:24:00 +0300

    task 5 added
    not tested

--- src/Classes/IncreasingSequence/Sequence.java
+++ src/Classes/IncreasingSequence/Sequence.java
@@ -1,19 +1,33 @@
+
 package Classes.IncreasingSequence;
 
 import java.util.ArrayList;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
 
-public class Sequence extends ArrayList<Integer>{
+public class Sequence extends ArrayList<Integer> {
 
-//    public int makeIncreasing() {
-//        for (int i = 0; i < this.size() - 1; i++) {
-//            boolean isIncreasing = false;
-//            int sequenceLength = 0;
-//            if (this.get(i) < this.get(i + 1)) {
-//                isIncreasing = true;
-//            }
-//            if (isIncreasing) {
-//                sequenceLength++;
-//            }
-//        }
-//    }
+    public int GetUselessElementsCount() {
+        int[] sequences = GetSequenceLengths();
+        OptionalInt max = IntStream.of(sequences).max();
+        return this.size() - max.getAsInt();
+    }
+
+    private int[] getInitializedLengthsArray (){
+        int[] lengths = new int[this.size()];
+        for (int i = 0; i < this.size(); i++ )
+            lengths[i] = 1;
+        return lengths;
+    }
+
+    private int[] GetSequenceLengths() {
+        int[] lengths = getInitializedLengthsArray();
+
+        for (int i = 1; i < this.size(); i++ )
+            for (int j = 0; j < i; j++ )
+                if ( this.get(i) > this.get(j) && lengths[i] < lengths[j] + 1)
+                    lengths[i] = lengths[j] + 1;
+        return lengths;
+    }
+
 }
