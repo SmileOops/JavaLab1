2c8b278280060fc888eb889282c204ce105b8442
Vlad <diablo3mail111@gmail.com>
2016-09-27 01:05:09 +0300

    added 7 task
    not tested

---
+++ src/Classes/SortableArray/SortableArray.java
@@ -0,0 +1,23 @@
+package Classes.SortableArray;
+
+import java.util.ArrayList;
+
+public class SortableArray extends ArrayList<Double> {
+
+    public void sortByShell() {
+        int i = 0;
+        while (i < this.size() - 1) {
+            if (this.get(i) <= this.get(i + 1)) {
+                i++;
+            }
+            else {
+                double temp = this.get(i);
+                this.set(i, this.get(i + 1));
+                this.set(i + 1, temp);
+                if (i != 0) {
+                    i--;
+                }
+            }
+        }
+    }
+}
