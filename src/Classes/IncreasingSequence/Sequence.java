458ecda434f554d04ae44598b9c2c1277ffbdbee
Vlad <diablo3mail111@gmail.com>
2016-09-26 10:50:54 +0300

    added fifth task
    not tested

--- src/Classes/IncreasingSequence/Sequence.java
+++ src/Classes/IncreasingSequence/Sequence.java
@@ -1,19 +1,44 @@
 package Classes.IncreasingSequence;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
-public class Sequence extends ArrayList<Integer>{
+public class Sequence extends ArrayList<Integer> {
 
-//    public int makeIncreasing() {
-//        for (int i = 0; i < this.size() - 1; i++) {
-//            boolean isIncreasing = false;
-//            int sequenceLength = 0;
-//            if (this.get(i) < this.get(i + 1)) {
-//                isIncreasing = true;
-//            }
-//            if (isIncreasing) {
-//                sequenceLength++;
-//            }
-//        }
-//    }
+    public int GetUselessElementsCount() {
+        int[] sequences = GetSequencesLength();
+        OptionalInt max = IntStream.of(sequences).max();
+        return this.size() - max.getAsInt();
+    }
+
+    private int[] GetSequencesLength(){
+        int[] result = new int[this.size()];
+        for (int i = 0; i < this.size() - 1; i++) {
+            result[i] = 1;
+            int interruptionIndex = 0;
+            boolean isInterrupted = false;
+            for (int j = i; j < this.size() - 1; j++) {
+                if (!isInterrupted) {
+                    if (this.get(j) < this.get(j + 1)) {
+                        result[i]++;
+                    }
+                    else {
+                        interruptionIndex = j;
+                        isInterrupted = true;
+                    }
+                }
+                else {
+                    if (this.get(interruptionIndex) < this.get(j + 1)) {
+                        isInterrupted = false;
+                        result[i]++;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
 }
