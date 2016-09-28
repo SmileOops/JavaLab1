a41abec541425f08a4a57a0ba9e5c4a63e454848
Vlad <diablo3mail111@gmail.com>
2016-09-28 12:46:16 +0300

    task 14 completed with test

---
+++ src/Tests/BookTest.java
@@ -0,0 +1,36 @@
+package Tests;
+
+import Classes.Book.Book;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+import static org.junit.Assert.*;
+
+public class BookTest {
+
+    private boolean isSorted(ArrayList<Book> books) {
+        for (int i = 0; i < books.size() - 1; i++) {
+            if (books.get(i).getIsbn().compareTo(books.get(i).getIsbn()) > 0) return false;
+        }
+
+        return true;
+    }
+
+    @Test
+    public void isSortingCorrect(){
+        ArrayList<Book> books = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < 50; i++) {
+            books.add(new Book("test", "test", 1, 2, "123-23223-" + random.nextInt(1000)+ "-6"));
+        }
+
+        Collections.sort(books);
+        Assert.assertTrue(isSorted(books));
+    }
+
+}
