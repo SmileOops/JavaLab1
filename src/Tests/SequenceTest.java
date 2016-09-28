a2d1dc03f56b3bcc67c3ec5f486bba9db7383ad5
Vlad <diablo3mail111@gmail.com>
2016-09-28 12:12:09 +0300

    task 5 completed with test

---
+++ src/Tests/SequenceTest.java
@@ -0,0 +1,27 @@
+package Tests;
+
+import Classes.IncreasingSequence.Sequence;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SequenceTest {
+    @Test
+    public void getUselessElementsCount() throws Exception {
+        Sequence sequence = new Sequence();
+        sequence.add(20);
+        sequence.add(40);
+        sequence.add(10);
+        sequence.add(12);
+        sequence.add(13);
+        sequence.add(11);
+        sequence.add(12);
+        sequence.add(13);
+        sequence.add(14);
+        sequence.add(5);
+
+        Assert.assertEquals(5, sequence.GetUselessElementsCount());
+    }
+
+}
